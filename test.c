88a79c0e6d97e939dca4d5dc0089a09ef4aa28dc
eXerigumo Clanjor (哆啦比猫) <cjxgm@126.com>
2013-02-08 21:18:07 +0800

    added unit tester

---
+++ test.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include "pack.h"
+
+int main()
+{
+	Pack * p = pack_new();
+	while (1) {
+		CREATE(int, n);
+		scanf("%d", n);
+		if (*n == 0) break;
+		pack_add_tail(p, n);
+	}
+	pack_walk(p, int, n, {
+		printf("%d\n", *n);
+	});
+	return 0;
+}
+
